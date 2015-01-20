2a11f685bf819e4b43a8f316115d52fced7e154e
Sigbjørn Tvedt <sigbjornt@gmail.com>
2015-01-20 22:09:00 +0100

    commited the script

---
+++ finnPublishedDate.sh
@@ -0,0 +1,28 @@
+####constants###
+url="http://www.finn.no/finn/realestate/homes/object?finnkode="
+start_element="<span class=\"last-changed-date\">"
+end_element="<\/span>"
+default_items=10
+
+###input parameters####
+#The object you want to see
+object_id=$1
+
+#Scan this many objects on either side. Ads may be deleted, so scan several
+steps=${2:-$default_items}
+
+
+
+#Calculate ranges
+start=$(($object_id - $steps))
+end=$(($object_id + $steps))
+
+#Loop over the sequence
+for i in $(seq $start 1 $end); do
+        #get the html,  use sed to find the last changed span and cut the  extra lines
+        str=$(curl -L  -s $url$i | sed -n "/$start_element/, /$end_element/p"  | sed '1d;$d')
+        #Print extra info for the primary object
+        if [ "$object_id" -eq "$i" ]; then echo  "Your item ($i)->       $str";
+        #Print the last changed date if found
+        elif [ ! -z "$str" -a "$str" != " " ]; then echo "Item $i where last updated $str";fi
+done
